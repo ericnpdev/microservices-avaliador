b2bed34982c4be646140f9e21b57ded314dba59b
Eric Nunes de Paula <ericnpdev@gmail.com>
2023-05-25 12:15:42 -0300

    criacao classes para SituacaoCliente no domain.model

---
+++ src/main/java/io/github/ericnpdev/msavaliadorcredito/domain/model/DadosCliente.java
@@ -0,0 +1,11 @@
+package io.github.ericnpdev.msavaliadorcredito.domain.model;
+
+import lombok.Data;
+
+@Data
+public class DadosCliente {
+
+    private Long id;
+    private String nome;
+
+}
---
+++ src/main/java/io/github/ericnpdev/msavaliadorcredito/domain/model/SituacaoCliente.java
@@ -0,0 +1,14 @@
+package io.github.ericnpdev.msavaliadorcredito.domain.model;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SituacaoCliente {
+
+    private DadosCliente cliente;
+    private List<CartaoCliente> cartoes;
+
+}
