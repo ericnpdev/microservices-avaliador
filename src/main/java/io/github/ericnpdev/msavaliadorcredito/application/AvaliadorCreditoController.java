16e1aaff9fa753aa50e7fc796c9e9e122cb318e3
Eric Nunes de Paula <ericnpdev@gmail.com>
2023-05-29 17:48:46 -0300

    att endpoint

--- src/main/java/io/github/ericnpdev/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ src/main/java/io/github/ericnpdev/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -3,14 +3,13 @@ package io.github.ericnpdev.msavaliadorcredito.application;
 
 import io.github.ericnpdev.msavaliadorcredito.application.ex.DadosClientNotFoundException;
 import io.github.ericnpdev.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
+import io.github.ericnpdev.msavaliadorcredito.domain.model.DadosAvaliacao;
+import io.github.ericnpdev.msavaliadorcredito.domain.model.RetornoAvaliacaoCliente;
 import io.github.ericnpdev.msavaliadorcredito.domain.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("avaliacoes-credito")
@@ -30,6 +29,18 @@ public class AvaliadorCreditoController {
         try {
             SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
             return ResponseEntity.ok(situacaoCliente);
+        } catch (DadosClientNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (ErroComunicacaoMicroservicesException e) {
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity realizarAvaliacao(@RequestBody DadosAvaliacao dados) {
+        try {
+            RetornoAvaliacaoCliente retornoAvaliacaoCliente = avaliadorCreditoService.realizarAvaliacao(dados.getCpf(), dados.getRenda());
+            return ResponseEntity.ok(retornoAvaliacaoCliente);
         } catch (DadosClientNotFoundException e) {
             return ResponseEntity.notFound().build();
         } catch (ErroComunicacaoMicroservicesException e) {
