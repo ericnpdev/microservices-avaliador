c9df4c1e47be44066a8a7b330b62ba6ff9c5b688
Eric Nunes de Paula <ericnpdev@gmail.com>
2023-05-29 17:45:55 -0300

    att endpoint

--- src/main/java/io/github/ericnpdev/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ src/main/java/io/github/ericnpdev/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -3,9 +3,7 @@ package io.github.ericnpdev.msavaliadorcredito.application;
 import feign.FeignException;
 import io.github.ericnpdev.msavaliadorcredito.application.ex.DadosClientNotFoundException;
 import io.github.ericnpdev.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
-import io.github.ericnpdev.msavaliadorcredito.domain.model.CartaoCliente;
-import io.github.ericnpdev.msavaliadorcredito.domain.model.DadosCliente;
-import io.github.ericnpdev.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.ericnpdev.msavaliadorcredito.domain.model.*;
 import io.github.ericnpdev.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.github.ericnpdev.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -41,4 +41,42 @@ public class AvaliadorCreditoService {
             throw new ErroComunicacaoMicroservicesException(e.getMessage(), status);
         }
     }
+
+    public RetornoAvaliacaoCliente realizarAvaliacao(String cpf, Long renda)
+            throws DadosClientNotFoundException, ErroComunicacaoMicroservicesException{
+        try {
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+            ResponseEntity<List<Cartao>> cartoesResponse = cartoesClient.getCartoesRendaAteh(renda);
+
+            List<Cartao> cartoes = cartoesResponse.getBody();
+            var listaCartoesAprovados = cartoes.stream().map(cartao ->  {
+
+                DadosCliente dadosCliente = dadosClienteResponse.getBody();
+
+                BigDecimal limiteBasico = cartao.getLimiteBasico();
+                BigDecimal rendaBD = BigDecimal.valueOf(renda);
+                BigDecimal idadeBD = BigDecimal.valueOf(dadosCliente.getIdade());
+                var fator = idadeBD.divide(BigDecimal.valueOf(10));
+                BigDecimal limiteAProvado = fator.multiply(limiteBasico);
+
+                CartaoAprovado aprovado = new CartaoAprovado();
+                aprovado.setCartao(cartao.getNome());
+                aprovado.setBandeira(cartao.getBandeira());
+                aprovado.setLimiteAprovado(limiteAProvado);
+
+                return aprovado;
+            }).collect(Collectors.toList());
+
+            return new RetornoAvaliacaoCliente(listaCartoesAprovados);
+
+        } catch (FeignException.FeignClientException e){
+            int status = e.status();
+            if (HttpStatus.NOT_FOUND.value() == status){
+                throw new DadosClientNotFoundException();
+            }
+            throw new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
+
+    }
+
 }
