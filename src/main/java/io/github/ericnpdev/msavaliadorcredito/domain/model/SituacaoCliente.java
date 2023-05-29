f00edc5ca44db56780998a168141fe034d153148
Eric Nunes de Paula <ericnpdev@gmail.com>
2023-05-29 12:55:02 -0300

    alteracoes

--- src/main/java/io/github/ericnpdev/msavaliadorcredito/domain/model/SituacaoCliente.java
+++ src/main/java/io/github/ericnpdev/msavaliadorcredito/domain/model/SituacaoCliente.java
@@ -1,11 +1,18 @@
 package io.github.ericnpdev.msavaliadorcredito.domain.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class SituacaoCliente {
 
     private DadosCliente cliente;
