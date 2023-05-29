6c189dde7a494d24b177d953edcb2c5f8be17ca1
Eric Nunes de Paula <ericnpdev@gmail.com>
2023-05-29 13:37:09 -0300

    enpoint status cliente e cartao try catch

--- src/main/java/io/github/ericnpdev/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ src/main/java/io/github/ericnpdev/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -1,16 +1,39 @@
 package io.github.ericnpdev.msavaliadorcredito.application;
 
 
+import io.github.ericnpdev.msavaliadorcredito.application.ex.DadosClientNotFoundException;
+import io.github.ericnpdev.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
+import io.github.ericnpdev.msavaliadorcredito.domain.model.SituacaoCliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("avaliacoes-credito")
+@RequiredArgsConstructor
 public class AvaliadorCreditoController {
+
+    private final AvaliadorCreditoService avaliadorCreditoService;
 
     @GetMapping
     public String status(){
+
         return "ok";
+    }
+
+    @GetMapping(value = "situacao-cliente", params = "cpf")
+    public ResponseEntity consultarSituacaoCliente(@RequestParam("cpf") String cpf){
+        try {
+            SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
+            return ResponseEntity.ok(situacaoCliente);
+        } catch (DadosClientNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (ErroComunicacaoMicroservicesException e) {
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
     }
 }
